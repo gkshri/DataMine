30344da7925366c41da90ff68b7cf91d03f36815
Gokul Shrikanth <35601856+gkshri@users.noreply.github.com>
2020-12-13 12:49:05 +0530

    Update datamine.py
    self.lock added for threading

--- datamine.py
+++ datamine.py
@@ -5,19 +5,25 @@ from functools import partial
 class datamine:
     Filepath="D:/demo.json"
     def __init__(self):
+        self.lock=threading.Lock()
         self.data={}
 
     def filepathcreate(self,var):
         self.Filepath=var
 
     def write(self,data):
+            self.lock.acquire()
             f=open(datamine.Filepath,"w")
+            print("writing to file")
+            print(datamine.Filepath)
             json_object = json.dumps(self.data, indent = 4)
             f.write(json_object)
             print("Changes commited")
+            self.lock.release();
             f.close()
 
     def create(self,key,value,timeout=0):
+            self.lock.acquire()
             if key in self.data:
                 print("error: this key already exists")
             else:
@@ -30,6 +36,7 @@ class datamine:
                         if len(key)<=32:
                             self.data[key]=l
                             print("Created")
+                            self.lock.release();
                             self.write(self.data)
                             print(self.data);
                             print("File modified")
@@ -38,7 +45,9 @@ class datamine:
                 else:
                     print("invalid key")
     def delete(self,key):
+            self.lock.acquire()
             if key not in self.data:
+                self.lock.release();
                 print("value not found")
             else:
                 b=self.data[key]
@@ -48,36 +57,49 @@ class datamine:
                         print("successfully deleted")
                         print("data after")
                         print(self.data)
+                        self.lock.release();
                         self.write(self.data)
                         print("File modified")
                     else:
+                        self.lock.release();
                         print("expired")
                 else:
                     del self.data[key]
                     print("else")
                     print("successfully deleted")
-                    self.write(self.data)        
+                    print("data after")
+                    print(self.data)
+                    self.lock.release();
+                    self.write(self.data)
+                    print("File modified")        
     def read(self,key):
+            self.lock.acquire()
             if key not in self.data:
                 print("value not found")
+                self.lock.release();
             else:
                 b=self.data[key]
                 if b[1]!=0:
                     if time.time()<b[1]:
                         stri=str(key)+":"+str(b[0])
+                        self.lock.release();
                         return stri
                     else:
+                        self.lock.release();
                         print("expired")
                 else:
                     stri=str(key)+":"+str(b[0])
+                    print("reading")
+                    self.lock.release();
                     return stri
 d=datamine()
 mylock = threading.Lock()
-print("enter the file path defaults to D:/demo.json")
+print("enter path defaults to D:/demo.json")
 var=input()
 d.filepathcreate(var)
-d.create("abc",10)
+d.create("abc",10,60)
 d.create("bat",20)
+print(d.read("abc"))
 print(d.read("bat"))
 d.delete("bat")
 
