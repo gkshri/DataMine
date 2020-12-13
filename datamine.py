ed2ac027cec80868e70baafd77460b3c4cd77d83
Gokul Shrikanth <35601856+gkshri@users.noreply.github.com>
2020-12-13 11:11:41 +0530

    Added ThreadSafe phase-1
    Made a class and prepared to add thread safety

--- datamine.py
+++ datamine.py
@@ -1,72 +1,79 @@
 import time
 import json
-data={}
-Filepath="D:/demo.json"
-def filecreate(Filepath="D:/demo.json"):
-    print("enter file path")
-    Filepath=input()
+import threading,_thread
+from functools import partial
+class datamine:
+    def __init__(self):
+        self.data={}
+        self.Filepath="D:/demo.json"
+    def filecreate(self,Filepath="D:/demo.json"):
+        print("enter file path")
+        self.Filepath=input()
 
-def write(data):
-    f=open(Filepath,"w")
-    json_object = json.dumps(data, indent = 4)
-    f.write(json_object)
-    print("Changes commited")
-    f.close()
+    def write(self,data):
+            f=open(self.Filepath,"w")
+            json_object = json.dumps(self.data, indent = 4)
+            f.write(json_object)
+            print("Changes commited")
+            f.close()
 
-def create(key,value,timeout=0):
-    if key in data:
-        print("error: this key already exists")
-    else:
-        if(key.isalpha()):
-            if len(data)<(1024*1020*1024) and value<=(16*1024*1024):
-                if timeout==0:
-                    l=[value,timeout]
+    def create(self,key,value,timeout=0):
+            if key in self.data:
+                print("error: this key already exists")
+            else:
+                if(key.isalpha()):
+                    if len(self.data)<(1024*1020*1024) and value<=(16*1024*1024):
+                        if timeout==0:
+                            l=[value,timeout]
+                        else:
+                            l=[value,time.time()+timeout]
+                        if len(key)<=32:
+                            self.data[key]=l
+                            print("Created")
+                            d=datamine()
+                            d.write(self.data)
+                            print("File modified")
+                        else:
+                            print("large file")
                 else:
-                    l=[value,time.time()+timeout]
-                if len(key)<=32:
-                    data[key]=l
-                    print("Created")
-                    write(data)
-                    print("File modified")
+                    print("invalid key")
+    def delete(self,key):
+            if key not in self.data:
+                print("value not found")
             else:
-                print("large file")
-        else:
-            print("invalid key")
-def delete(key):
-    if key not in data:
-        print("value not found")
-    else:
-        b=data[key]
-        if b[1]!=0:
-            if time.time()<b[1]:
-                del data[key]
-                print("successfully deleted")
-                write(data)
-                print("File modified")
+                b=self.data[key]
+                if b[1]!=0:
+                    if time.time()<b[1]:
+                        del self.data[key]
+                        print("successfully deleted")
+                        d=datamine()
+                        d.write(self.data)
+                        print("File modified")
+                    else:
+                        print("expired")
+                else:
+                    del self.data[key]
+                    print("successfully deleted")        
+    def read(self,key):
+            if key not in self.data:
+                print("value not found")
             else:
-                print("expired")
-        else:
-            del data[key]
-            print("successfully deleted")        
-def read(key):
-    if key not in data:
-        print("value not found")
-    else:
-        b=data[key]
-        if b[1]!=0:
-            if time.time()<b[1]:
-                stri=str(key)+":"+str(b[0])
-                return stri
-            else:
-                print("expired")
-        else:
-            stri=str(key)+":"+str(b[0])
-            return stri
-
-# filecreate()
-# create("abc",10)
-# create("bat",20)
-# print(read("bat"))
-# delete("abc")
+                b=self.data[key]
+                if b[1]!=0:
+                    if time.time()<b[1]:
+                        stri=str(key)+":"+str(b[0])
+                        return stri
+                    else:
+                        print("expired")
+                else:
+                    stri=str(key)+":"+str(b[0])
+                    return stri
+d=datamine()
+mylock = threading.Lock()
+d.filecreate()
+d.create("abc",10)
+d.create("bat",20)
+print(d.read("bat"))
+d.delete("abc")
 
     
