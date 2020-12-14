18819579b10edd05ec2475b6f09f7b289004b543
Gokul Shrikanth <35601856+gkshri@users.noreply.github.com>
2020-12-14 13:04:00 +0530

    Update datamine.py

--- datamine.py
+++ datamine.py
@@ -2,34 +2,57 @@ import time
 import json
 import threading,_thread
 class datamine:
-    Filepath="D:/demo.json"
-    def __init__(self):
+    Filepath="D:/demo.json"                       #File path
+    def __init__(self,data={}):                   #constructor
         self.lock=threading.Lock()
-        self.data={}
+        self.data=data
 
     def filepathcreate(self,var):
         if var=='':
-            datamine.Filepath="D:/demo.json"
+            datamine.Filepath="D:/demo.json"      #default filepath
         else:
             datamine.Filepath=var
-
-    def write(self,data):
-            self.lock.acquire()
+    
+    def searchfromJSON(self):
+            data=self.readJSON()
+            d=datamine(data)
+            d.read("abc")                          #use read here to read from the JSON file
+            
+            
+    def write(self,data):                          #Write to file
+            self.lock.acquire()                    #self lock for accessing the resource
             f=open(datamine.Filepath,"w")
-            print("writing to file")
+            print("writing to file-")
             print(datamine.Filepath)
             json_object = json.dumps(self.data, indent = 4)
             f.write(json_object)
             print("Changes commited")
-            self.lock.release();
+            self.lock.release()
             f.close()
 
-    def create(self,key,value,timeout=0):
+    def readJSON(self):                            #Read a JSON file
+            self.filepathcreate()
+            f=open(datamine.Filepath,"r")
+            data=json.load(f)
+            print("reading from JSON file")
+            print(data)
+            return(data)
+
+    def writeJSONobj(self,JSONobj={}):              #Write a json File to the dict
+            f=open(datamine.Filepath,"a")
+            print("writing JSON object to file")
+            for key in JSONobj:
+                b=JSONobj.get(key)
+                self.create(str(key),str(b[0]),str(0))
+            f.close()
+
+
+    def create(self,key,value,timeout=0):           # create an entry in the JSON file, We can use TTL if needed as a 3rd param
             if key in self.data:
                 print("error: this key already exists")
             else:
                 if(key.isalpha()):
-                    if len(self.data)<(1024*1020*1024) and value<=(16*1024*1024):
+                    if len(self.data)<(1024*1020*1024) and value<=(16*1024*1024): # data<1GB and JSONobj < 32chars
                         if timeout==0:
                             l=[value,timeout]
                         else:
@@ -44,7 +67,8 @@ class datamine:
                             print("large file")
                 else:
                     print("invalid key")
-    def delete(self,key):
+
+    def delete(self,key):                             # Delete an entry from the file 
             if key not in self.data:
                 print("value not found")
             else:
@@ -66,8 +90,9 @@ class datamine:
                     print("data after")
                     print(self.data)
                     self.write(self.data)
-                    print("File modified")        
-    def read(self,key):
+                    print("File modified")      
+
+    def read(self,key):                                 # Reading from the file
             self.lock.acquire()
             if key not in self.data:
                 print("value not found")
@@ -87,12 +112,18 @@ class datamine:
                     print("reading")
                     self.lock.release();
                     return stri
+
+# create a class object to access the functions
 d=datamine()
 print("enter path defaults to D:/demo.json")
 var=input()
 d.filepathcreate(var)
 d.create("abc",10,60)
 d.create("bat",20)
+d.readJSON()
+d.searchfromJSON()
+dic={'pine': [10,0]}
+d.writeJSONobj(dic)
 print(d.read("abc"))
-print(d.read("bat"))
-d.delete("bat")
+print(d.read("pine"))
+#d.delete("bat")
